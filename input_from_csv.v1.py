e241d4946466fd2fe793528bdc3957ed5b65f9e2
yujianmin <jxyyjm@gmail.com>
2018-12-13 21:45:59 +0800

    input from queue

--- input_from_csv.v1.py
+++ input_from_csv.v1.py
@@ -20,13 +20,14 @@ def Get_files_path(file_path=''):
 	return csv_file_names
 
 csv_file_names = Get_files_path()
-# create a FIFO queue #
+# 1) create a FIFO queue #
 filename_queue = tf.train.string_input_producer(csv_file_names)
 print ('filename_queue: ', filename_queue)
+# 1.1) Reader 
 reader = tf.TextLineReader()
 print ('reader: ', reader)
 key, value = reader.read(filename_queue)
-
+# 1.2) Parse line
 record_defaults = [[] for i in range(785)] # notice : tf.decode_csv return like this format-type #
 print ('record_defaults: ', record_defaults)
 parse_record_op = tf.decode_csv(value, record_defaults = record_defaults, field_delim=',') # return a list #
@@ -34,12 +35,15 @@ print ('parse_record: ', len(parse_record_op))
 #feature        = parse_record[1:]
 #label          = parse_record[0]
 
+# 2) Paras define
 random_par = tf.Variable(tf.random_normal(shape=(2,3), mean=0, stddev=1.0, dtype=tf.float32))
 zeros_par  = tf.Variable(tf.zeros(shape=(2,3), dtype=tf.float32))
 
 init_op    = tf.global_variables_initializer()
 
-with tf.Session() as sess:
+# 3) Session run
+gpu_options = tf.GPUOptions(allow_growth=True)
+with tf.Session(config = tf.ConfigProto(gpu_options=gpu_options)) as sess:
 	sess.run(init_op)
 	print ('random_par: ', sess.run(random_par))
 	print ('zeros_par:  ', sess.run(zeros_par))
